79396572ec8982e6b8a6a9a183b9e3f299815c58
yuvashre2103 <yuvashre2103@gmail.com>
2025-09-09 20:52:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',              // make sure vite knows root
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     proxy: {
